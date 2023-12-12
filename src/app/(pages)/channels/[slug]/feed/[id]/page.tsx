3e3ef05112e76337dc67e26f89bd68e18549b2ac
Muhammad Zeeshan <57444568+MuhammadZeeshan03@users.noreply.github.com>
2023-12-12 11:44:52 +0500

    Comment removed

--- src/app/(pages)/channels/[slug]/feed/[id]/page.tsx
+++ src/app/(pages)/channels/[slug]/feed/[id]/page.tsx
@@ -1,14 +1,4 @@
 import Post from '@/components/shared/post'
-
-// const SingleFeed = ({ params }: any) => {
-//     const id = params.id
-//     console.log(id)
-//     return <Post postId={id} />
-// }
-
-// export default SingleFeed
-
-
 import React from 'react'
 
 function page({ params }: any) {
