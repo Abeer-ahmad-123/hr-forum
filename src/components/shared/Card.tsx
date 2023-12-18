3d2c7a706bf292f116751cf25ddc3b7c9ad130ab
macbook <macbook@macbooks-MacBook-Pro.local>
2023-12-18 17:47:45 +0500

    Feed card design changed

--- src/components/shared/Card.tsx
+++ src/components/shared/Card.tsx
@@ -36,50 +36,65 @@ const Card = ({ post, channels }: any) => {
               : ` /feeds/feed/${id}`
           }>
           <div className="px-10 py-4">
-            <div className="mb-8 mt-3 text-left text-4xl font-semibold dark:text-white">
-              {title}
-            </div>
-            <div className="flex flex-col">
-              <div className="flex items-center justify-between max-md:block">
-                <div className="flex items-center">
-                  <div className="-z-2">
-                    <div className="static rounded-xl ">
-                      <Image
-                        className="inline-block w-[48px] rounded-full object-contain ring-2 ring-white dark:ring-gray-800"
-                        width={48}
-                        height={48}
-                        src={user?.profile_picture_url}
-                        alt="user-picture"
-                      />
-                    </div>
-                  </div>
-                  <div className="ml-2 text-sm">
-                    <div className="ml-4 flex flex-col items-start">
-                      <p
-                        className="text-[16px] font-light leading-none text-gray-900 dark:text-gray-300"
-                        aria-label="user-name">
-                        {user?.name}
-                      </p>
-                      <p className="text-slate-500 dark:text-gray-400">
-                        {timeFormatInHours(created_at)}
-                      </p>
-                    </div>
+
+            <div className="flex items-center justify-between max-md:block">
+              <div className="flex items-center">
+                <div className="-z-2">
+                  <div className="static rounded-xl ">
+                    <Image
+                      className="inline-block rounded-full object-contain ring-2 ring-white dark:ring-gray-800"
+                      width={32}
+                      height={32}
+                      src={user?.profile_picture_url}
+                      alt="user-picture"
+                    />
                   </div>
                 </div>
 
-                <div className="flex items-end pb-2 pt-4">
-                  <div className="flex flex-col sm:flex-row">
+
+                <div className="ml-2 flex flex-col align-baseline items-start">
+                  <div className='flex flex-row'>
+                    <Link
+                      href={`/profile/${user?.id}`}>
+                      <p
+                        className="font-normal w-full text-sm leading-none text-gray-900 hover:bg-gray-200  dark:text-gray-300"
+                        aria-label="user-name">
+                        {user?.name}
+
+                        {/* Yogesh Choudhary Paliyal */}
+                      </p>
+                    </Link>
+
                     <ChannelPill channel_id={channel_id} channels={channels} />
+
                   </div>
+
+                  <p className="text-slate-500 font-light text-xs dark:text-gray-400">
+                    {timeFormatInHours(created_at)}
+                  </p>
                 </div>
+
               </div>
+
+              <div className="flex items-end pb-2 pt-4">
+
+              </div>
+            </div>
+
+
+            <div className="flex flex-col">
+              <div className="my-3 text-left text-3xl font-semibold dark:text-white">
+                {title}
+              </div>
+
+
               <div
-                className="pt-8 text-left text-gray-700 dark:text-gray-300"
+                className="text-left text-gray-700 dark:text-gray-300"
                 dangerouslySetInnerHTML={{ __html: content }}
               />
             </div>
           </div>
-        </Link>
+        </Link >
 
         <PostReactionBar reaction_summary={reaction_summary} postId={id} />
         <hr />
@@ -87,7 +102,7 @@ const Card = ({ post, channels }: any) => {
         <div className="py-2">
           <PostActionBar linkToFeed={`/feeds/feed/${id}`} postId={id} />
         </div>
-      </div>
+      </div >
     </>
   )
 }
--- src/components/shared/ChannelPill.tsx
+++ src/components/shared/ChannelPill.tsx
@@ -4,11 +4,13 @@ const ChannelPill = ({ channel_id, channels }: any) => {
   const channelObj = arrayToKeyIdNValueData(channels)
 
   return (
-    <span
-      aria-label="channel-name"
-      className={`border-1 border-1 mb-2 mr-2 inline-block whitespace-nowrap rounded-lg bg-indigo bg-opacity-30 px-3 py-1 text-sm font-semibold text-indigo ring-1 ring-indigo dark:bg-dark-background-secondary dark:text-white`}>
-      {channelObj[channel_id]?.name}
-    </span>
+    // <span
+    //   aria-label="channel-name"
+    //   className={`border-1 border-1 mb-2 mr-2 inline-block whitespace-nowrap rounded-lg bg-indigo bg-opacity-30 px-3 py-1 text-sm font-semibold text-indigo ring-1 ring-indigo dark:bg-dark-background-secondary dark:text-white`}>
+    //   {channelObj[channel_id]?.name}
+    // </span>
+
+    <span className='text-slate-500 font-light text-xs dark:text-gray-400 px-1'>Posted in {channelObj[channel_id]?.name}</span>
   )
 }
 
