ab49aafa8a9d9c50f4d1a02ad4e671c36dc2a23d
mac <mac@MacBook-Pro.local>
2023-12-18 19:57:27 +0500

    Resposnive Feed Page Design

--- src/components/Cards/ResponsiveChannelCard.tsx
+++ src/components/Cards/ResponsiveChannelCard.tsx
@@ -3,11 +3,11 @@ import React from "react";
 import { useSelector } from 'react-redux'
 
 
-const ChannelCard = () => {
+const ResChannelCard = () => {
     const channels = useSelector((state) => state?.channels?.channels);
     return (
         <>
-            <div className="w-[200px] mt-[25px] pt-3 pb-2 mr-4 bg-white dark:bg-slate-800 dark:text-white shadow-lg  h-auto rounded-[10px] cursor-cursor sticky max-h-screen top-0  ">
+            <div className="w-auto mt-5 mb-5 pt-3 pb-2  bg-white dark:bg-slate-800 dark:text-white shadow-lg  h-auto rounded-[10px] cursor-cursor sticky max-h-screen top-0  ">
                 <h1 className="text-[17px] font-medium justify-center text-center mt-[10px] mb-[20px]">
                     THE CHANNELS
                 </h1>
@@ -31,4 +31,4 @@ const ChannelCard = () => {
         </>
     );
 };
-export default ChannelCard;
+export default ResChannelCard;
