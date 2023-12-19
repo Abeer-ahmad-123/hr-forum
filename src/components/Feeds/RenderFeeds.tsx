fd7ea3da7571c358ab537da2599d553330a88388
Muhammad Zeeshan <mzeeshan.bscs19seecs@seecs.edu.pk>
2023-12-19 14:32:43 +0500

    RenderFeed Page design issue resolved

--- src/components/Feeds/RenderFeeds.tsx
+++ src/components/Feeds/RenderFeeds.tsx
@@ -9,15 +9,12 @@ import { getChannelIdByChannelName } from '@/utils/channels'
 import ProfileCard from '@/components/SideCards/ProfileCard'
 import RulesCard from '@/components/SideCards/RuleCard'
 import ChannelCard from '@/components/SideCards/ChannelCard'
-import ProfileCardSkelton from '../ProfileCardSkelton'
-import ChannelCardSkelton from '../ChannelCardSkelton'
-import RulesCardSkelton from '../RuleCardSkelton'
 
 import RespScreen from '../Cards/ResponsiveScreen'
 
 
 async function RenderFeeds({ channelSlug = null }) {
-  
+
   const { channels } = await getChannels()
   let initialPosts = []
   let morePosts = true
@@ -38,76 +35,58 @@ async function RenderFeeds({ channelSlug = null }) {
     initialPosts = data?.posts
     morePosts = data?.pagination?.CurrentPage !== data?.pagination?.TotalPages
   }
- 
+
   return (
-    <div className='flex'> 
+    <div className='flex justify-center'>
       <div className='flex ml-20 mt-5 flex-col lg:block max-sm:hidden max-md:hidden'>
-          <ProfileCard />
-          <div className='sticky max-h-screen top-0' style={{ top: '60px' }}>
-            <ChannelCard /></div>
-        </div>
-       
-    <div className=''>
-
-      <p className="mb-4 text-3xl my-5 text-black dark:text-white">
-        {!!channelSlug &&
-          toPascalCase(channelSlug?.toString()?.replaceAll('-', ' '))}
-      </p>
-      <div className='flex justify-center'>
-<<<<<<< HEAD
-
-        <div className='flex flex-col'>
-          <ProfileCard />
-          <div className='sticky max-h-screen top-0' style={{ top: '35px' }}>
-            <ChannelCard />
-          </div>
-        </div>
-        <div>
-          <div className="mx-auto max-w-screen-lg dark:text-white">
-=======
-        {/* <div className='flex flex-col lg:block max-sm:hidden max-md:hidden'>
-          <ProfileCard />
-          <div className='sticky max-h-screen top-0' style={{ top: '35px' }}>
-            <ChannelCard /></div>
-        </div> */}
-        {/*  put the responsive profile compoennt here */}
-        <div>
-          
-<div className='lg:hidden max-sm:block md:hidden'> <RespScreen/>
-</div>
-<div className="lg:hidden max-sm:block md:hidden mb-5"> <PostBar/></div>
-
-
-          <div className="mx-auto max-w-screen-lg dark:text-white">
-          <div className='mb-5 max-sm:hidden max-md:hidden' >
-          <PostBar />
-        </div>
->>>>>>> feedDesign
-            <Feeds
-              channelSlug={channelSlug}
-              initialPosts={initialPosts}
-              channels={channels}
-              morePosts={morePosts}
-            />
-          </div>
-
-<<<<<<< HEAD
-        </div>
-        <div className='sticky max-h-screen ' style={{ top: '60px' }}>
-          <RulesCard />
-        </div>
-=======
-        {/* <div className='sticky max-h-screen lg:block max-sm:hidden sm:hidden ' style={{ top: '60px' }}> <RulesCard />
-        </div> */}
->>>>>>> feedDesign
+        <ProfileCard />
+        <div className='sticky max-h-screen top-0' style={{ top: '60px' }}>
+          <ChannelCard /></div>
       </div>
 
+      <div className=''>
+
+        <p className="mb-4 text-3xl my-5 text-black dark:text-white">
+          {!!channelSlug &&
+            toPascalCase(channelSlug?.toString()?.replaceAll('-', ' '))}
+        </p>
+        <div className='flex justify-center'>
+          {/* <div className='flex flex-col lg:block max-sm:hidden max-md:hidden'>
+          <ProfileCard />
+          <div className='sticky max-h-screen top-0' style={{ top: '35px' }}>
+            <ChannelCard /></div>
+        </div> */}
+          {/*  put the responsive profile compoennt here */}
+          <div>
+
+            <div className='lg:hidden max-sm:block md:hidden'> <RespScreen />
+            </div>
+            <div className="lg:hidden max-sm:block md:hidden mb-5"> <PostBar /></div>
 
 
-    </div>
+            <div className="mx-auto max-w-screen-lg dark:text-white">
+              <div className='mb-5 max-sm:hidden max-md:hidden' >
+                <PostBar />
+              </div>
+              <Feeds
+                channelSlug={channelSlug}
+                initialPosts={initialPosts}
+                channels={channels}
+                morePosts={morePosts}
+              />
+            </div>
+          </div>
 
-        <div className='sticky mt-5 max-h-screen lg:block max-sm:hidden sm:hidden ' style={{ top: '60px' }}> <RulesCard />
+          {/* <div className='sticky max-h-screen lg:block max-sm:hidden sm:hidden ' style={{ top: '60px' }}> <RulesCard />
+        </div> */}
         </div>
+
+
+
+      </div>
+
+      <div className='sticky mt-5 max-h-screen lg:block max-sm:hidden sm:hidden ' style={{ top: '60px' }}> <RulesCard />
+      </div>
     </div>
   )
 }
