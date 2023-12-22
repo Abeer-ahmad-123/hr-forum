a3503b17a0e155d707ac07855f1da48cdc8110de
Muhammad Zeeshan <mzeeshan.bscs19seecs@seecs.edu.pk>
2023-12-22 20:57:18 +0500

    ScrollBar Hidden

--- src/components/ui/Dialog/interceptDialog.tsx
+++ src/components/ui/Dialog/interceptDialog.tsx
@@ -1,8 +1,9 @@
 'use client'
 
-import * as React from 'react'
 import * as DialogPrimitive from '@radix-ui/react-dialog'
 import { X } from 'lucide-react'
+import * as React from 'react'
+import './style.css'
 
 import { cn } from '@/lib/utils'
 import { useRouter } from 'next/navigation'
@@ -46,11 +47,10 @@ const DialogContent = React.forwardRef<
         ref={ref}
         className={cn(
           //removed snadow-lg
-          'fixed left-[50%] top-[50%] z-50 mx-auto grid max-h-[80vh] min-h-[60vh] w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] dark:bg-dark-background sm:rounded-lg md:w-full',
+          'hide-scrollbar fixed left-[50%] top-[50%] z-50 mx-auto grid max-h-[80vh] min-h-[60vh] w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] dark:bg-dark-background sm:rounded-lg md:w-full',
           className,
         )}
-        {...props}
-      >
+        {...props}>
         {children}
         <button className="focus:ring-ring data-[state=open]:text-muted-foreground absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent">
           <X className="h-4 w-4 dark:text-white" onClick={close} />
@@ -119,13 +119,13 @@ DialogDescription.displayName = DialogPrimitive.Description.displayName
 
 export {
   Dialog,
-  DialogPortal,
-  DialogOverlay,
   DialogClose,
-  DialogTrigger,
   DialogContent,
-  DialogHeader,
-  DialogFooter,
-  DialogTitle,
   DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogOverlay,
+  DialogPortal,
+  DialogTitle,
+  DialogTrigger,
 }
