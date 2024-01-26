c6ae2281395443d11ef6f5042bf20d9a7b5a759c
abeer-ahmad-123 <abeerahmad@devryx.com>
2024-01-26 18:49:27 +0500

    Added dropdown from medium screens and remove profile card from medium to small screens and there loadings

--- src/components/Cards/ResponsiveScreen.tsx
+++ src/components/Cards/ResponsiveScreen.tsx
@@ -1,46 +1,19 @@
 'use client'
-import { LoggedInUser } from '@/utils/interfaces/loggedInUser'
-import { useState } from 'react'
-import { FaAngleDown, FaAngleUp } from 'react-icons/fa6'
-import { useSelector } from 'react-redux'
-import ChannelCard from './ChannelCard'
-import ProfileCard from './ProfileCard'
+import { Suspense } from 'react'
+import FeaturesDropDown from '../Navbar/FeaturesDropdown'
+import Skelton from '../ui/skelton'
 
 const RespScreen = () => {
-  const [showComponent, setShowComponent] = useState(false)
-  const loggedInUseraDetails = useSelector(
-    (state: LoggedInUser) => state?.loggedInUser?.userData,
-  )
-
-  const handleClick = () => {
-    setShowComponent(!showComponent)
-  }
-
   return (
-    <>
-      <div>
-        {loggedInUseraDetails.id && <ProfileCard />}
-        {showComponent && <ChannelCard />}
-        <div className="pointer mb-5 w-full">
-          <div className="text-gray-500" onClick={handleClick}>
-            {' '}
-            {showComponent ? (
-              <div className="flex flex-row items-center justify-center gap-x-2">
-                Hide Details
-                <FaAngleUp />
-              </div>
-            ) : (
-              <div className="flex flex-row items-center justify-center gap-x-2">
-                Show More{' '}
-                <span>
-                  <FaAngleDown />
-                </span>
-              </div>
-            )}{' '}
-          </div>
-        </div>
-      </div>
-    </>
+    <div className="hidden max-md:block">
+      <Suspense fallback={<Skelton className="w-[300px h-8 bg-skelton" />}>
+        <FeaturesDropDown
+          classNameOuter="w-[300px] bg-white"
+          classNameInner="w-[300px]"
+          classNamefeaturesDropDowm="w-[300px]"
+        />
+      </Suspense>
+    </div>
   )
 }
 export default RespScreen
--- src/components/Loading/cardLoading.tsx
+++ src/components/Loading/cardLoading.tsx
@@ -27,7 +27,7 @@ function CardLoading() {
       <div
         className={`mt-8 flex justify-center max-md:mt-5  max-md:block max-md:w-full`}>
         <div className={`flex flex-col ${hidden ? ' pr-4' : ''}`}>
-          {token && <ProfileCardSkelton className={'max-md:w-full'} />}
+          {token && <ProfileCardSkelton className={'max-md:hidden'} />}
           <Skelton className="mx-auto mt-2 hidden h-6  w-24 rounded-sm bg-skelton max-md:block" />
 
           <div
