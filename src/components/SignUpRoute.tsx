8f62eda36b87d34d8e3514d799ba9e1fbc336de8
Muhammad Zeeshan <mzeeshan.bscs19seecs@seecs.edu.pk>
2024-01-27 00:00:51 +0500

    redirect to feeds if already logged in

--- src/components/SignUpRoute.tsx
+++ src/components/SignUpRoute.tsx
@@ -1,10 +1,13 @@
 'use client'
 import Signup from '@/components/Signup'
+import { LoggedInUser } from '@/utils/interfaces/loggedInUser'
 import { usePathname, useRouter } from 'next/navigation'
 import nProgress from 'nprogress'
 import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 
 const SignUpRoute = () => {
+  const token = useSelector((state: LoggedInUser) => state?.loggedInUser?.token)
   const pathName = usePathname()
   const router = useRouter()
 
@@ -15,6 +18,10 @@ const SignUpRoute = () => {
   }
 
   useEffect(() => {
+    if (token) {
+      router.replace('/feeds')
+    }
+
     return () => {
       nProgress.done()
     }
--- src/components/signInRoute.tsx
+++ src/components/signInRoute.tsx
@@ -1,10 +1,13 @@
 'use client'
 import Signin from '@/components/Signin'
+import { LoggedInUser } from '@/utils/interfaces/loggedInUser'
 import { usePathname, useRouter } from 'next/navigation'
 import nProgress from 'nprogress'
 import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 
 const SignInRoute = () => {
+  const token = useSelector((state: LoggedInUser) => state?.loggedInUser?.token)
   const pathName = usePathname()
   const router = useRouter()
 
@@ -16,6 +19,10 @@ const SignInRoute = () => {
   }
 
   useEffect(() => {
+    if (token) {
+      router.replace('/feeds')
+    }
+
     return () => {
       nProgress.done()
     }
