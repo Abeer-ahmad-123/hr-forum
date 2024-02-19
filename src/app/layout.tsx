8b7fddd587c9ac36eac872534b932a1a3d9e2748
abeer-ahmad-123 <abeerahmad@devryx.com>
2024-02-19 12:31:58 +0500

    Updated favico link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,9 @@ export const metadata: Metadata = {
   ],
   viewport: { width: 'device-width', initialScale: 1 },
   robots: { index: true, follow: true },
-  icons: ['@/app/favicon.ico'],
+  icons: [
+    'https://h-forum.s3.us-east-2.amazonaws.com/uploads/1708327881-favicon.ico/c7007038-abb1-45d0-8f09-0d21bd475017.ico',
+  ],
 
   openGraph: {
     title: 'HR-Forum',
