383167a484ef15b7a3c290050ff2a633489647eb
abeer-ahmad-123 <abeerahmad@devryx.com>
2024-02-25 12:43:06 +0500

    Fixed channel card height issue

--- src/components/Cards/ChannelCard.tsx
+++ src/components/Cards/ChannelCard.tsx
@@ -16,7 +16,7 @@ const ChannelCard = () => {
 
   return channels.length ? (
     <div
-      className={`sticky top-[20px] mr-4 mt-[20px] h-[260px] max-h-screen w-[225px] overflow-auto rounded-[10px] border border-solid border-gray-300 bg-white px-[10px] pb-2 pt-3 shadow-lg dark:bg-slate-800 dark:text-white max-lg:mt-[15px] max-md:w-full`}>
+      className={`sticky top-[20px] mr-4 mt-[20px] max-h-[260px] w-[225px] overflow-auto rounded-[10px] border border-solid border-gray-300 bg-white px-[10px] pb-2 pt-3 shadow-lg dark:bg-slate-800 dark:text-white max-lg:mt-[15px] max-md:w-full`}>
       <h1 className="mb-[0px] mt-[10px] text-start text-[11px] font-semibold">
         Channels
       </h1>
