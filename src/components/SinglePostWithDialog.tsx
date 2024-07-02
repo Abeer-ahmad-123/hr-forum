ae06a4a27a0f0806a8de4f4b686e54854e25ae77
Zain Haroon <77679885+ZH1245@users.noreply.github.com>
2024-07-02 12:53:41 +0500

    refactor(modal-feeds): :zap: removed cookie based feed modal and updated it to use open close using escape key

--- src/components/SinglePostWithDialog.tsx
+++ src/components/SinglePostWithDialog.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Post from '@/components/shared/post'
 import PostSkelton from '@/components/shared/post/PostSkelton'
 import {
@@ -5,14 +6,35 @@ import {
   DialogContent,
   DialogDescription,
 } from '@/components/ui/Dialog/interceptDialog'
-import { cookies } from 'next/headers'
+import {
+  CommentInterface,
+  Pagination,
+  PostsInterface,
+} from '@/utils/interfaces/posts'
+import { useRouter } from 'next/navigation'
 import { Suspense } from 'react'
 
-const SinglePostWithDialog = ({ id, data }: any) => {
-  const modalState = cookies().get('modal')?.value
+export type SinglePostWithDialogProps = {
+  id: string
+  data?: {
+    post: PostsInterface
+    comments: {
+      comments: CommentInterface[]
+      pagination: Pagination
+    }
+  }
+}
+const SinglePostWithDialog = ({ id, data }: SinglePostWithDialogProps) => {
+  // const modalState = cookies().get('modal')?.value
+  const router = useRouter()
+  function handleCloseModal(value: boolean) {
+    if (!value) {
+      router.back()
+    }
+  }
 
   return (
-    <Dialog defaultOpen={Boolean(modalState)}>
+    <Dialog defaultOpen={true} open={true} onOpenChange={handleCloseModal}>
       <DialogContent className="max-w-5xl bg-white ">
         <DialogDescription>
           <Suspense fallback={<PostSkelton isDialogPost={true} />}>
