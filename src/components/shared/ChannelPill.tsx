3c62380c854057f3fe1d5b743eaaf392b27fe5de
Zain Haroon <zain.haroon@enxsys.com>
2024-07-11 13:42:46 +0500

    fix(card): :bug: Fixed a bug with card when clicked on channel then it would not open due to stopPropagation so removed it

--- src/components/shared/ChannelPill.tsx
+++ src/components/shared/ChannelPill.tsx
@@ -5,8 +5,8 @@ import {
   ChannelByIdInterface,
   ChannelInterface,
 } from '@/utils/interfaces/channels'
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
-import { CustomLink } from './customLink/CustomLink'
 interface ChannelObject {
   [key: string]: {
     name?: string
@@ -22,6 +22,11 @@ const ChannelPill = ({ channel_id, channels }: Props) => {
   const channelObj: ChannelObject = arrayToKeyIdNValueData(channels)
   const lowerCaseChannelName = channelObj[channel_id]?.name?.toLowerCase()
 
+  // ! Was causing navigation issues on post.
+  /**
+   *
+   * @deprecated Use `<Link>` instead
+   */
   const handleLinkClick = (event: any) => {
     event.preventDefault()
     event.stopPropagation() // Prevent propagation to card's onClick
@@ -31,18 +36,14 @@ const ChannelPill = ({ channel_id, channels }: Props) => {
   return (
     <span
       className="text-xs font-light
-    text-slate-500 dark:text-gray-400 max-custom-sm:text-[11px] max-[392px]:text-[10px] max-custom-sx:text-[8px] md:text-xs lg:text-sm xl:text-sm"
-    >
+    text-slate-500 dark:text-gray-400 max-custom-sm:text-[11px] max-[392px]:text-[10px] max-custom-sx:text-[8px] md:text-xs lg:text-sm xl:text-sm">
       Posted in
-      <CustomLink className="" href={`/channels/${lowerCaseChannelName}/`}>
+      <Link className="" href={`/channels/${lowerCaseChannelName}/`}>
         {` `}
-        <span
-          className="font-medium  text-gray-900 hover:underline dark:text-white"
-          onClick={handleLinkClick}
-        >
+        <span className="font-medium  text-gray-900 hover:underline dark:text-white">
           {channelObj[channel_id]?.name}
         </span>
-      </CustomLink>
+      </Link>
     </span>
   )
 }
