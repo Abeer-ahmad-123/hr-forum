efcd97b447ca4303bdf88139c2d815358354efe1
Muhammad Zeeshan <mzeeshan.bscs19seecs@seecs.edu.pk>
2024-09-13 14:40:19 +0500

    loading skelton added

--- src/components/Cards/ResponsiveScreen.tsx
+++ src/components/Cards/ResponsiveScreen.tsx
@@ -1,13 +1,13 @@
 'use client'
 import { Suspense } from 'react'
 import FeaturesDropDown from '../Navbar/FeaturesDropdown'
-import Skelton from '../ui/skelton'
 import CustomAccordion from '../CustomAccordion'
+import RespScreenLoading from '../Loading/RespScreenLoading'
 
 const RespScreen = () => {
   return (
-    <div className="max-md: hidden w-full max-md:block max-[640px]:mt-3">
-      <Suspense fallback={<Skelton className="h-8 w-[300px] bg-skelton" />}>
+    <div className="max-md: hidden w-full max-md:block">
+      <Suspense fallback={<RespScreenLoading />}>
         <FeaturesDropDown
           classNameOuter="w-[300px] max-md:w-full"
           classNameInner="w-[300px] max-md:w-full "
