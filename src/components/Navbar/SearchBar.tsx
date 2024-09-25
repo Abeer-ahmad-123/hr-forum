00dca4d664d9060b786df31b78db1e15fd5e3103
abeer-ahmad-123 <abeerahmad@devryx.com>
2024-09-25 17:34:30 +0500

    UI fixes

--- src/components/Navbar/SearchBar.tsx
+++ src/components/Navbar/SearchBar.tsx
@@ -37,14 +37,14 @@ const SearchBar = () => {
   }, [SearchParams])
 
   return (
-    <form className="relative flex h-10 w-10 max-w-[420px] flex-1 items-center space-x-2 rounded-full border border-none border-gray-200 bg-bg-tertiary py-1 pl-3 outline-none transition-all duration-700 ease-in-out focus-within:w-full hover:border-blue-500 focus:w-full dark:border-gray-600 dark:bg-bg-tertiary-dark dark:hover:border-gray-400 dark:hover:bg-bg-tertiary-dark max-sm:max-w-[160px] md:h-11 md:w-11">
+    <form className="relative flex h-10 w-10 max-w-[420px] flex-1 items-center space-x-2 rounded-full border border-none border-gray-200 bg-bg-tertiary py-1 pl-3 outline-none focus-within:w-full hover:border-blue-500 focus:w-full dark:border-gray-600 dark:bg-bg-tertiary-dark dark:hover:border-gray-400 dark:hover:bg-bg-tertiary-dark max-sm:max-w-[160px] md:h-11 md:w-11">
       <input
         ref={refForInput}
         onKeyDown={handleKeyDown}
         onChange={handleSearch}
         value={search}
         type="text"
-        className="ml-4 rounded-r-full border border-none bg-bg-tertiary pl-3 font-light text-black placeholder-opacity-60 outline-none transition-all duration-700 ease-in-out placeholder:text-base placeholder:font-medium dark:bg-bg-tertiary-dark dark:text-white max-md:w-0 focus:max-sm:w-full sm:w-full md:ml-8 md:pl-2"
+        className="ml-4 rounded-r-full border border-none bg-bg-tertiary pl-3 font-light text-black placeholder-opacity-60 outline-none placeholder:text-base placeholder:font-medium dark:bg-bg-tertiary-dark dark:text-white max-md:w-0 focus:max-sm:w-full sm:w-full md:ml-8 md:pl-2"
         placeholder="Search Forum"
       />
       <label className="absolute left-[-6px] max-sm:left-[-3px]">
@@ -52,7 +52,7 @@ const SearchBar = () => {
           className="pointer-events-auto flex cursor-pointer items-center rounded-full bg-transparent"
           onClick={handleIconClick}>
           <div className={`mr-5 flex h-7 rounded-lg`}>
-            <div className="flex items-center justify-center pl-0 transition-all duration-700 ease-in-out sm:pl-[3px] md:ml-[10px]">
+            <div className="flex items-center justify-center pl-0 sm:pl-[3px] md:ml-[10px]">
               <SearchIcon className="fill-current text-black dark:text-white md:text-color-grey dark:md:text-color-grey" />
             </div>
           </div>
